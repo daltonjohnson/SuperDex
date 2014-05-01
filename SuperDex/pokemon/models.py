14664eb6fe88927b3936d7a9071212ff0703501b
Jeffrey Zhu <jeffreyzhu88@gmail.com>
2014-05-01 16:22:03 -0400

    Added relations to model,  validated

--- SuperDex/pokemon/models.py
+++ SuperDex/pokemon/models.py
@@ -6,8 +6,8 @@ class Pokemon(models.Model):
 	name = models.CharField(max_length=50)
 	nickname = models.CharField(max_length=50)
 	number = models.IntegerField(primary_key=True)
-	imageurl = models.URLField
-	poke_type = models.ForeignKey('poke_type')
+	imageurl = models.URLField()
+	#poke_type = models.ForeignKey('poke_type')
 	genderratio = models.CharField(max_length=30)
 	catchrate = models.CharField(max_length=30)
 	egggroups = models.CharField(max_length=30)
@@ -15,65 +15,52 @@ class Pokemon(models.Model):
 	height_m = models.CharField(max_length=20)
 	weight_lbs = models.CharField(max_length=20)
 	weight_kg = models.CharField(max_length=20)
-	evhp = models.IntegerField
-	evattack = models.IntegerField
-	evdefense = models.IntegerField
-	evspattack = models.IntegerField
-	evdefense = models.IntegerField
-	evspeed = models.IntegerField
-	basefriendship = models.IntegerField
-	basehp = models.IntegerField
-	baseattack = models.IntegerField
-	basedefense = models.IntegerField
-	basespattack = models.IntegerField
-	basespdefense = models.IntegerField
-	basespeed = models.IntegerField
-	basetotal models.IntegerField
-	def __str__(self):
-		return self.name
+	evhp = models.IntegerField()
+	evattack = models.IntegerField()
+	evdefense = models.IntegerField()
+	evspattack = models.IntegerField()
+	evdefense = models.IntegerField()
+	evspeed = models.IntegerField()
+	basefriendship = models.IntegerField()
+	basehp = models.IntegerField()
+	baseattack = models.IntegerField()
+	basedefense = models.IntegerField()
+	basespattack = models.IntegerField()
+	basespdefense = models.IntegerField()
+	basespeed = models.IntegerField()
+	basetotal = models.IntegerField()
 
-class poke_type(models.Model):
+#class poke_type(models.Model):
 
 class Ability(models.Model):
 	ability_id = models.IntegerField(primary_key=True)
 	name = models.CharField(max_length = 20)
 	description = models.CharField(max_length=200)
-	def __str__(self):
-		return self.name
 
 class has_ability(models.Model):
 	ability_id = models.ForeignKey(Ability)
 	pokemon_id = models.ForeignKey(Pokemon)
-	def __str__(self):
-		return self.ability_id
 
 class Moves(models.Model):
 	move_id = models.IntegerField(primary_key=True)
 	name = models.CharField(max_length=30)
 	move_type = models.CharField(max_length=15)
 	category = models.CharField(max_length=15)
-	pp = models.IntegerField
+	pp = models.IntegerField()
 	power = models.CharField(max_length=15)
 	accuracy = models.CharField(max_length=15)
-	gen = = models.CharField(max_length=5)
-	def __str__(self):
-		return self.name
+	gen = models.CharField(max_length=5)
 
 class learnset(models.Model):
 	pokemon_id = models.ForeignKey(Pokemon)
 	move_id = models.ForeignKey(Moves)
-	level = models.IntegerField
-	def __str__(self):
-		return self.move_id
+	level = models.IntegerField()
 
-class can_learn
+class can_learn(models.Model):
 	pokemon_id = models.ForeignKey(Pokemon)
-	move_id = models.ForeignKey(move)
-	def __str__(self):
-		return self.pokemon_id
+	move_id = models.ForeignKey(Moves)
 
 class Evolution(models.Model):
-	pokemon_id1 = models.ForeignKey(Pokemon)
-	pokemon_id2 = models.ForeignKey(Pokemon)
+	pokemon_id1 = models.ForeignKey(Pokemon, related_name='pokemon_1')
+	pokemon_id2 = models.ForeignKey(Pokemon, related_name='pokemon_2')
 	how = models.CharField(max_length=20)
-	
